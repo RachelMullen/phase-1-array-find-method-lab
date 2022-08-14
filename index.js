f36c270eedb08746275a44e36e91469ce48655e2
Rachel Mullen <mullen.rachel@gmail.com>
2022-08-14 13:12:37 -0600

    commit tree 0d5c60bf37ec1e9c82704796d1663c93b37a79eb

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+// function superbowlWin([year], [result]) {
+//     return (year, result === "W")
+// }
+
+superbowlWin = (record) => {
+    const result = record.find(record => record.result === "W");
+    return result ? result.year : undefined;
+}
